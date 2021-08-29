0de5b12daf17758a3d36deee4728a69786c8e93c
raphael <raphael@mcledger.com>
2021-08-29 20:21:05 +0200

    Add modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="modal">
+		<div class="modal-content">
+			<div class="title">
+				<slot name="title" />
+				<Icon icon="close-circle-outline" @click="$emit('close-modal')" />
+			</div>
+			<div class="content">
+				<slot name="content" />
+			</div>
+		</div>
+		<div class="modal-backdrop" @click="$emit('close-modal')" />
+	</div>
+</template>
+<style scoped>
+.modal {
+	position: fixed;
+	z-index: 4;
+	height: 100vh;
+	width: 100vw;
+	top: 0;
+	right: 0;
+	margin: 0 !important;
+}
+.modal-backdrop {
+	position: absolute;
+	height: 100vh;
+	width: 100vw;
+	top: 0;
+	right: 0;
+	background-color: var(--black);
+	opacity: 0.5;
+}
+.modal-content {
+	width: 90%;
+	max-width: 500px;
+	background-color: var(--white);
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	z-index: 5;
+	border-radius: var(--border-radius);
+	transform: translate(-50%, -50%);
+}
+.title {
+	background-color: var(--primary);
+	color: var(--white);
+	border-top-left-radius: var(--border-radius);
+	border-top-right-radius: var(--border-radius);
+	font-size: 1.2em;
+	font-weight: bold;
+	padding: 10px 20px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+.title:deep(.icon) {
+	cursor: pointer;
+}
+.content {
+	padding: 10px 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import i18n from './translations/i18n.js'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import setRgbaColors from './utils/color'
-import Spinner from './components/Spinner'
+import Modal from './components/Modal'
+import Icon from './components/Icon'
 const app = createApp(App)
-app.component('Spinner', Spinner)
+app.component('Modal', Modal)
+app.component('Icon', Icon)
 setRgbaColors()
 app.use(VueAxios, axios)
 app.use(router)
